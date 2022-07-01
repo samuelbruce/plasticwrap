a11d36a3fa4e91624f4a1b4c2cdeaca69b738a21
Samuel Bruce <11494533+samuelbruce@users.noreply.github.com>
2022-07-01 12:46:13 +1000

    Changeset: 1 Initial commit

---
+++ plasticwrap.py
@@ -0,0 +1,57 @@
+
+import json
+import requests
+import subprocess
+import sys
+import time
+
+class PlasticWrap:
+    def __init__(self, api_url="http://localhost:9090/api/v1"):
+        self.api_url = api_url
+        self.api_process = None
+        # try a simple request, invoke cm api if it fails
+        try:
+            requests.get(api_url)
+        except requests.ConnectionError:
+            print("Starting Plastic API")
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags = subprocess.STARTF_USESTDHANDLES | subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+            self.api_process = subprocess.Popen(r"cm api", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo)
+            time.sleep(2.5)
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.api_process is not None:
+            self.api_process.terminate()
+    
+    def get_repos(self):
+        url = self.api_url + "/repos"
+        req = requests.get(url)
+        if req.status_code == 200:
+            return req.json()
+        return None
+    
+    def get_workspaces(self):
+        url = self.api_url + "/wkspaces"
+        req = requests.get(url)
+        if req.status_code == 200:
+            return req.json()
+        return None
+    
+    def get_switch_status(self, workspace):
+        url = self.api_url + "/wkspaces/" + workspace + "/switch"
+        req = requests.get(url)
+        if req.status_code == 200:
+            return json.loads(req.content)["status"]
+        return None
+    
+    def switch_workspace(self, workspace, objectType, object):
+        url = self.api_url + "/wkspaces/" + workspace + "/switch"
+        params = {"objectType": objectType, "object": object}
+        req = requests.post(url, json=params)
+        if req.status_code == 200:
+            return True
+        return None
